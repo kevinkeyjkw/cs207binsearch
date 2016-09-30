8d19ab122cdad24974357c6c50de1486650fcba0
kevin <kevinkeyjkw@gmail.com>
2016-09-29 23:54:31 -0400

    sdf

--- test_binsearch.py
+++ test_binsearch.py
@@ -2,7 +2,7 @@ from pytest import raises
 from binsearch import binary_search
 
 ordered = [1,2,5,7,8,9]
-multiple = [1,1,1,1,2,2,2,3]
+multiple = [1,1,2,3]
 unordered = [2,5,3,5,3,4,8]
 nothing = []
 one = [1]
@@ -23,43 +23,36 @@ def test_nonnumeric():
     with raises(TypeError):
     	binary_search(nonNumeric, 5) == 2
 
-
 def test_empty():
-    # Empty array - -1
     assert binary_search(nothing, 4)==-1
 
 def test_nan():
-    # nan - -1
-
     assert binary_search(nan, 1) == 0
-#     # boundaries
-#     binary_search(ordered, 1)
-#     binary_search(ordered, 9)
-#     # Out of range needle
-#     binary_search(ordered, 10)
-#     binary_search(ordered, 0)
+
+def test_negative():
+	assert binary_search(negative, -4) == 1
+
+def test_bound_left():
+	assert binary_search(ordered, 1) == 0
+
+def test_bound_right():
+	assert binary_search(ordered, 9) == 5
+
+def test_out_of_range():
+	assert binary_search(ordered, 10) == -1
+
+def test_out_of_range():
+	assert binary_search(ordered, 0) == -1
+
+# def test_multiple_found():
+# 	assert binary_search(multiple, 1) == 5
+
 #     # multiple needles found
 #     binary_search(multiple, 1)
 #     # rangemax overflow
 #     binary_search(ordered, 1, left=0, right=float("inf")+1)
 
 
-# def test_left1():
-# 	with raises(RuntimeError):
-# 		binary_search(ordered, 1,left=-1, right=-2)
-
-
-# def test_char():
-#     with raises(TypeError):
-#         myaverage(['a',3])
-
-# def test_mymath():
-#     assert mymedian([9,3, 6]) == 6
-    
-# def test_zero_median():
-#     with raises(ValueError):
-#         mymedian([])
-        
-# def test_char_median():
-#     with raises(TypeError):
-#         mymedian(['a', 3])
+def test_char():
+    with raises(TypeError):
+        binary_search(nonNumeric, 3)
